268bc9be926435ea535dac0ad382cbdc19e18042
kavalar <apuc06@mail.ru>
2019-04-03 10:54:47 +0200

    Посадил страницы

--- common/models/Experience.php
+++ common/models/Experience.php
@@ -1,6 +1,7 @@
 <?php
 namespace common\models;
 
+use phpDocumentor\Reflection\Types\Array_;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
@@ -13,14 +14,15 @@ use yii\db\ActiveRecord;
  * @property string $city
  * @property string $post
  * @property string $responsibility
- * @property string $month_from
- * @property string $month_to
- * @property string $year_from
- * @property string $year_to
+ * @property integer $month_from
+ * @property integer $month_to
+ * @property integer $year_from
+ * @property integer $year_to
  * @property string $department
  * @property integer $status
  * @property integer $created_at
  * @property integer $updated_at
+ * @property string $period_string
  *
  * @property Resume $resume
  */
@@ -96,4 +98,87 @@ class Experience extends ActiveRecord
     {
         return $this->hasOne(Resume::className(), ['id' => 'resume_id']);
     }
+
+    /**
+     * @return array
+     */
+    public function getPeriod(){
+        $years = $this->year_to - $this->year_from;
+        if($this->month_to >= $this->month_from){
+            $months = $this->month_to - $this->month_from;
+        } else {
+            $years--;
+            $months = 12 - ($this->month_from - $this->month_to);
+        }
+        if($years < 0) $years = 0;
+        return [
+            'years' => $years,
+            'months' => $months
+        ];
+    }
+
+
+    /**
+     * @param array $period
+     * @return string
+     */
+    public static function getPeriod_string($period)
+    {
+        $result = '';
+        if($period['years'] > 0) {
+            $result .= $period['years'];
+            if ($period['years'] > 10 && $period['years'] < 20)
+                $result .= ' лет ';
+            else if ($period['years'] % 10 === 0)
+                $result .= ' лет ';
+            else if ($period['years'] % 10 === 1)
+                $result .= ' год ';
+            else if ($period['years'] % 10 >= 2 && $period['years'] % 10 <= 4)
+                $result .= ' года ';
+            else if ($period['years'] % 10 >= 5 && $period['years'] % 10 <= 9)
+                $result .= ' лет ';
+        }
+
+        if($period['months'] > 0) {
+            $result .= $period['months'];
+            if ($period['months'] > 10 && $period['months'] < 20)
+                $result .= ' месяцев ';
+            else if ($period['months'] % 10 === 0)
+                $result .= ' месяцев ';
+            else if ($period['months'] % 10 === 1)
+                $result .= ' месяц ';
+            else if ($period['months'] % 10 >= 2 && $period['months'] % 10 <= 4)
+                $result .= ' месяца ';
+            else if ($period['months'] % 10 >= 5 && $period['months'] % 10 <= 9)
+                $result .= ' месяцев ';
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param Experience[] $experiences
+     * @return array
+     */
+    public static function getPeriod_sum($experiences)
+    {
+        $result = [
+            'years' => 0,
+            'months' => 0
+        ];
+        if ($experiences) {
+            foreach ($experiences as $experience) {
+                $period = $experience->getPeriod();
+                $result['years'] += $period['years'];
+                $result['months'] += $period['months'];
+            }
+        }
+        $months_over = (int)($result['months'] / 12);
+        if ($months_over > 0) {
+            $result['months'] -= 12 * $months_over;
+            $result['years'] += $months_over;
+        }
+        return $result;
+    }
+
 }
