2431f79ac995b6f1197c048bbee8f6bc5d2bb35b
iskybow <Dt3i2k4w7>
2019-05-14 16:27:03 +0300

    профиль

--- frontend/web/vue/src/components/EditProfile.vue
+++ frontend/web/vue/src/components/EditProfile.vue
@@ -21,7 +21,9 @@
             this.formData.first_name = response.data[0].first_name;
             this.formData.second_name = response.data[0].second_name;
             this.formData.email = response.data[0].email;
-            this.formData.phone = response.data[0].phone.number;
+            if(response.data[0].phone == null) {
+							this.formData.phone = response.data[0].phone.number;
+						}
             this.idEmployer = response.data[0].id;
           }, response => {
           }
