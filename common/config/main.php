24bb1272dddc27ecd58b5258de9029a0972aef8c
dzavgo <apuc06@mail.ru>
2019-06-11 13:38:50 +0300

    Настроил SMTP

--- common/config/main.php
+++ common/config/main.php
@@ -3,20 +3,23 @@ return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
-        //'@dektrium/user' => '@vendor/dektrium/yii2-user'
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-//        'view' => [
-//            'theme' => [
-//                'pathMap' => [
-//                    //'@dektrium/user/views' => '@backend/views'
-//                ],
-//            ],
-//        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'mail.adm.tools',
+                'username' => 'noreply@rabota.today',
+                'password' => '123edsaqw',
+                'port' => '25',
+                'encryption' => 'tls',
+            ],
+        ],
     ],
     'modules' => [
         'User' => [
