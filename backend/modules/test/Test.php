f7ec83965c524a48530741b3e94c025f793d7fc1
dzavgo <apuc06@mail.ru>
2019-10-23 12:31:49 +0300

    Добавил в админке ссылку для подтверждения пользователя

---
+++ backend/modules/test/Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\test;
+
+/**
+ * test module definition class
+ */
+class Test extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\test\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
