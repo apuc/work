e0c4e3098bc5c3c437d870a5b86da46b6fe4c972
dzavgo <apuc06@mail.ru>
2020-01-09 17:54:08 +0300

    sitemap

--- frontend/controllers/SitemapController.php
+++ frontend/controllers/SitemapController.php
@@ -70,10 +70,6 @@ class SitemapController extends Controller
         Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
         $headers = Yii::$app->response->headers;
         $headers->add('Content-Type', 'text/xml');
-//        Yii::$app->getResponse()
-//            ->getHeaders()
-//            ->set('Content-Type', 'application/xml;');
-        Yii::$app->response->setStatusCode(200);
 
         return $xml_sitemap;
     }
--- frontend/views/sitemap/index.php
+++ frontend/views/sitemap/index.php
@@ -5,15 +5,15 @@
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach ($urls as $url): ?>
         <url>
-        <loc><?= $url['loc']; ?></loc>
-        <?php if (isset($url['lastmod'])): ?>
-            <lastmod><?= $url['lastmod']; ?></lastmod>
-        <?php endif; ?>
-        <changefreq><?= $url['changefreq']; ?></changefreq>
-        <priority><?= $url['priority']; ?></priority>
+            <loc><?= $url['loc']; ?></loc>
+            <?php if (isset($url['lastmod'])): ?>
+                <lastmod><?= $url['lastmod']; ?></lastmod>
+            <?php endif; ?>
+            <changefreq><?= $url['changefreq']; ?></changefreq>
+            <priority><?= $url['priority']; ?></priority>
         </url>
     <?php endforeach; ?>
-</urlset>​
+</sitemapindex>
