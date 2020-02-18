9c69e18ae1d4945f2fb16b4ba3a994ebf43274e5
crazykoha <crazykoha@gmail.com>
2020-02-18 16:07:01 +0300

    Правка

--- console/migrations/m200218_125423_alter_vacancy_table.php
+++ console/migrations/m200218_125423_alter_vacancy_table.php
@@ -15,6 +15,10 @@ class m200218_125423_alter_vacancy_table extends Migration
         $empty_category = new \common\models\Category();
         $empty_category->name = 'Пустая категория';
         $empty_category->save();
+        $this->update('vacancy',
+            ['main_category_id' => $empty_category->id],
+            ['main_category_id' => null]
+        );
         $this->alterColumn('vacancy', 'main_category_id', $this->integer()->notNull()->defaultValue($empty_category->id));
     }
 
@@ -23,6 +27,6 @@ class m200218_125423_alter_vacancy_table extends Migration
      */
     public function safeDown()
     {
-
+        $this->delete('category', ['name'=>"Пустая категория"]);
     }
 }
