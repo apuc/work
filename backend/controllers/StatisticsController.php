3dfc6ff0c7da00ff0a147b6f62bf1122ed7f4ccc
crazykoha <crazykoha@gmail.com>
2020-03-20 15:43:28 +0300

    Статистика в админке

---
+++ backend/controllers/StatisticsController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace backend\controllers;
+
+use common\classes\Debug;
+use common\models\Resume;
+use common\models\Vacancy;
+use common\models\Views;
+use yii\web\Controller;
+
+class StatisticsController extends Controller
+{
+    public static $types = [
+        1=>'Созданные вакансии',
+        2=>'Созданные резюме',
+        3=>'Просмотры вакансий',
+        4=>'Просмотры резюме'
+    ];
+
+    public function actionIndex(){
+        $date1 = \Yii::$app->request->get('date1');
+        $date2 = \Yii::$app->request->get('date2')?:'today';
+        if(!$date1)
+            $datedif = 10;
+        else {
+            $datedif = (strtotime($date2) - strtotime($date1))/86400+1;
+        }
+        $dates = ['dates'=>[], 'vacancies'=>[], 'label'=>''];
+        $type = \Yii::$app->request->get('type')?:1;
+        $view_type = \Yii::$app->request->get('view_type')?:0;
+        switch ($type) {
+            default:
+                $dates['label'] = 'Созданные вакансии';
+                break;
+            case 2:
+                $dates['label'] = 'Созданные резюме';
+                break;
+            case 3:
+                $dates['label'] = 'Просмотры вакансий';
+                break;
+            case 4:
+                $dates['label'] = 'Просмотры резюме';
+                break;
+        }
+        for ($i=$datedif-1;$i>=0;$i--) {
+            $beginOfDay = strtotime("today", strtotime($date2)-86400*$i);
+            $endOfDay   = strtotime("tomorrow", $beginOfDay) - 1;
+            $dates['dates'][] = date('d.m.Y', strtotime($date2)-86400*$i);
+
+            $query = null;
+            switch ($type){
+                default:
+                    $query = Vacancy::find()->where(['>', 'created_at', $beginOfDay])->andWhere(['<', 'created_at', $endOfDay]);
+                    break;
+                case 2:
+                    $query = Resume::find()->where(['>', 'created_at', $beginOfDay])->andWhere(['<', 'created_at', $endOfDay]);
+                    break;
+                case 3:
+                    $query = Views::find()->where(['>', 'dt_view', $beginOfDay])->andWhere(['<', 'dt_view', $endOfDay])->andWhere(['subject_type'=>'Vacancy']);
+                    if($view_type == 1)
+                        $query->andWhere('viewer_id IS NOT NULL');
+                    if($view_type == 2)
+                        $query->andWhere('viewer_id IS NULL');
+                    break;
+                case 4:
+                    $query = Views::find()->where(['>', 'dt_view', $beginOfDay])->andWhere(['<', 'dt_view', $endOfDay])->andWhere(['subject_type'=>'Resume']);
+                    if($view_type == 1)
+                        $query->andWhere('viewer_id IS NOT NULL');
+                    if($view_type == 2)
+                        $query->andWhere('viewer_id IS NULL');
+                    break;
+        }
+            $dates['data'][] = $query->count();
+        }
+        return $this->render('index', [
+            'dates' => $dates,
+            'type' => $type
+        ]);
+    }
+}
---
+++ backend/views/statistics/index.php
@@ -0,0 +1,88 @@
+<?php
+/* @var $this View */
+/* @var $dates array */
+/* @var $type integer */
+
+use backend\controllers\StatisticsController;
+use kartik\date\DatePicker;
+use yii\helpers\Html;
+use yii\web\View;
+
+$this->registerJsFile('/secure/js/Chart.bundle.min.js', ['position'=>View::POS_HEAD]);
+$this->registerJsFile('/secure/js/statisticsPageScript.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->title = "Статистика: " . StatisticsController::$types[$type];
+?>
+<?= Html::beginForm('', 'GET');?>
+<?= Html::dropDownList('type', $type, StatisticsController::$types, ['class'=>'form-control', 'id'=>'chart_type'])?>
+<br>
+<?= Html::dropDownList('view_type',
+    Yii::$app->request->get('view_type')?:0,
+    ['', 'Авторизованные пользователи', 'Неавторизованные пользователи'],
+    [
+        'class'=> ($type==3 || $type==4)?'form-control':'form-control hidden',
+        'id'=>'view_type'
+    ])
+?>
+<br>
+<?=Html::label('От:', 'date1')?>
+<?= DatePicker::widget([
+    'name'=>'date1',
+    'value'=>Yii::$app->request->get('date1')?:date('d.m.Y', time()-777600),
+    'pluginOptions' => [
+        'format' => 'dd.mm.yyyy',
+        'todayHighlight' => true
+    ]
+])?>
+<br>
+<?=Html::label('До:', 'date2')?>
+<?= DatePicker::widget([
+    'name'=>'date2',
+    'value'=>Yii::$app->request->get('date2')?:date('d.m.Y'),
+    'pluginOptions' => [
+        'format' => 'dd.mm.yyyy',
+        'todayHighlight' => true
+    ]
+])?>
+<br>
+<?=Html::submitButton('Применить', ['class'=>'btn btn-success'])?>
+<div style="width: 600px;height: 400px">
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                <?php foreach ($dates['dates'] as $date)
+                    echo "'$date', ";
+                ?>
+            ],
+            datasets: [{
+                label: '<?=$dates['label']?>',
+                data: [
+                    <?php foreach ($dates['data'] as $count)
+                    echo "$count, ";
+                    ?>
+                ],
+                backgroundColor: [
+                    <?php for ($i=0;$i<count($dates['data']);$i++):?>
+                    'rgba(<?=rand(0,255)?>, <?=rand(0,255)?>, <?=rand(0,255)?>, 0.5)',
+                    <?php endfor;?>
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+        }
+    });
+</script>
+</div>
+
+<?= \yii\helpers\Html::endForm();?>
