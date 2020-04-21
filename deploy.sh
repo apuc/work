67e54a4cc10571f54db0751666698fcbdc1dd30a
kavalar <apuc06@mail.ru>
2020-04-21 15:59:55 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
-cd ../../
-ls -la
-git pull origin master
+cd /var/www/work.art-craft.xyz/
+#ls -la
+
+echo "----git status----\n"
+git status
+
+echo "-----git pull-----\n"
+#git add .
+#git commit -m "merge"
+git pull
+
+echo "-----migrate-----\n"
 php yii migrate --interactive=0
+
+echo "-----minify-----\n"
 php yii minify
-cd frontend/web/vue
+
+cd /var/www/work.art-craft.xyz/frontend/web/vue
 sh build.sh
