fc34964a22fe65771eb576d21e765dabaacef0c1
crazykoha <crazykoha@gmail.com>
2020-04-21 16:27:12 +0300

    Правка

--- console/controllers/MinifyController.php
+++ console/controllers/MinifyController.php
@@ -17,9 +17,9 @@ class MinifyController extends Controller
         $this->minifyJS(Yii::getAlias("@frontend/web/js/jquery.sticky-kit.js"), Yii::getAlias("@frontend/web/js/jquery.sticky-kit.min.js"));
 
         $this->minifyCSS(Yii::getAlias("@frontend/web/css/main_style.css"), Yii::getAlias("@frontend/web/css/main_style.min.css"));
-        $this->minifyCSS(Yii::getAlias("@frontend/web/css/back_styles.css"), Yii::getAlias("@frontend/web/css/back_styles.min.css"));
+        $this->minifyCSS(Yii::getAlias("@frontend/web/css/back-styles.css"), Yii::getAlias("@frontend/web/css/back-styles.min.css"));
         $this->minifyCSS(Yii::getAlias("@frontend/web/css/style.css"), Yii::getAlias("@frontend/web/css/style.min.css"));
-        $this->minifyCSS(Yii::getAlias("@frontend/web/js/slick.css"), Yii::getAlias("@frontend/web/js/slick.min.css"));
+        $this->minifyCSS(Yii::getAlias("@frontend/web/js/slick/slick.css"), Yii::getAlias("@frontend/web/js/slick/slick.min.css"));
     }
     public function minifyJS($sourcePath, $minifiedPath) {
         (new Minify\JS($sourcePath))->minify($minifiedPath);
