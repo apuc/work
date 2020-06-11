2d8d222544fc286f4ba35149f6e7cb47e12687ca
crazykoha <crazykoha@gmail.com>
2020-06-11 12:54:53 +0300

    Метод для переноса компании

--- frontend/modules/request/controllers/CompanyController.php
+++ frontend/modules/request/controllers/CompanyController.php
@@ -13,9 +13,17 @@ use common\models\Resume;
 use common\models\ResumeSkill;
 use common\models\Skill;
 use common\models\UserCompany;
+use common\models\Vacancy;
+use common\models\VacancyCategory;
+use common\models\VacancySkill;
+use dektrium\user\events\FormEvent;
+use dektrium\user\models\Token;
 use dektrium\user\models\User;
+use frontend\controllers\RegistrationController;
+use frontend\models\user\RegUserForm;
 use Yii;
 use yii\base\InvalidConfigException;
+use yii\base\UserException;
 use yii\data\ActiveDataProvider;
 use yii\db\ActiveRecord;
 use yii\web\HttpException;
@@ -154,5 +162,70 @@ class CompanyController extends MyActiveController
             throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
         }
         return $model;
+    }
+
+    public function actionTransfer(){
+        $email = Yii::$app->request->post('email');
+        $company_id = Yii::$app->request->post('company_id');
+        if(!$email)
+            throw new UserException('Введите email');
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            throw new UserException('Неверный email');
+        if(!$company_id)
+            throw new UserException('Выберите компанию');
+        /** @var Company $company */
+        $company = Company::find()->where(['id'=>$company_id, 'status'=>Company::STATUS_ACTIVE])->one();
+        if(!$company)
+            throw new UserException('Такой компании не существует');
+        /** @var User $user */
+        $user = \common\models\User::find()->where(['email'=>$email])->one();
+        $vacancy_ids = Vacancy::find()->where(['company_id'=>$company->id])->column();
+        if($user) {
+            if((int)Company::find()->where(['owner'=>$user->id, 'status'=>Company::STATUS_ACTIVE])->count()>0) {
+                throw new UserException('У этого пользователя уже есть компания');
+            }
+            $company->owner = $user->id;
+            $company->user_id = $user->id;
+            $company->save();
+            if($vacancy_ids)
+                Vacancy::updateAll(['owner' => $user->id], ['id' => $vacancy_ids]);
+            Phone::updateAll(['owner'=>$user->id], ['company_id'=>$company->id]);
+            VacancyCategory::updateAll(['owner'=>$user->id], ['vacancy_id' => $vacancy_ids]);
+            VacancySkill::updateAll(['owner'=>$user->id], ['vacancy_id' => $vacancy_ids]);
+        } else {
+            $regForm = \Yii::createObject(RegUserForm::className());
+            $status = ($company->name=="" || $company->name==null)?21:20;
+            $data = [
+                'email'=>$email,
+                'username'=>$email,
+                'password'=>'g8f74n',
+                'status'=>$status
+            ];
+            if ( $regForm->load($data, '') && $regForm->register()) {
+                /** @var User $user */
+                $user = User::find()->where(['email' => $email])->one();
+                $employer = new Employer();
+                $employer->user_id = $user->id;
+                $employer->owner = $user->id;
+                $employer->save();
+                $token = Token::findOne(['user_id'=>$user->id]);
+                Yii::$app->mailer->viewPath='@common/mail';
+                Yii::$app->mailer->compose('registration_notification', ['employer'=>$employer, 'user'=>$user, 'token'=>$token])
+                    ->setFrom('noreply@rabota.today')
+                    ->setTo($user->email)
+                    ->setSubject('Спасибо за регистрацию')
+                    ->send();
+            } else return $regForm->errors;
+            $vacancy_ids = Vacancy::find()->where(['company_id'=>$company->id])->column();
+            $company->owner = $user->id;
+            $company->user_id = $user->id;
+            $company->save();
+            if($vacancy_ids)
+                Vacancy::updateAll(['owner' => $user->id], ['id' => $vacancy_ids]);
+            Phone::updateAll(['owner'=>$user->id], ['company_id'=>$company->id]);
+            VacancyCategory::updateAll(['owner'=>$user->id], ['vacancy_id' => $vacancy_ids]);
+            VacancySkill::updateAll(['owner'=>$user->id], ['vacancy_id' => $vacancy_ids]);
+        }
+        return 'success';
     }
 }
