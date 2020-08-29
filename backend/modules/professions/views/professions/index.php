d24387c9ea2e7d6dbd807fa96edfa40454a78f1b
kavalar <apuc06@mail.ru>
2020-08-29 13:44:35 +0300

    Правки в админке

--- backend/modules/professions/views/professions/index.php
+++ backend/modules/professions/views/professions/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use kartik\dynagrid\DynaGrid;
-use kartik\grid\GridView;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
--- backend/views/layouts/content.php
+++ backend/views/layouts/content.php
@@ -5,26 +5,10 @@ use dmstr\widgets\Alert;
 ?>
 <div class="content-wrapper">
     <section class="content-header">
-        <?php if (isset($this->blocks['content-header'])) { ?>
-            <h1><?= $this->blocks['content-header'] ?></h1>
-        <?php } else { ?>
-            <h1>
-                <?php
-                if ($this->title !== null) {
-                    echo \yii\helpers\Html::encode($this->title);
-                } else {
-                    echo \yii\helpers\Inflector::camel2words(
-                        \yii\helpers\Inflector::id2camel($this->context->module->id)
-                    );
-                    echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
-                } ?>
-            </h1>
-        <?php } ?>
-
         <?=
         Breadcrumbs::widget(
             [
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [], 'options' => ['class' => 'breadcrumb', 'style' => 'float:none; position:initial']
             ]
         ) ?>
     </section>
