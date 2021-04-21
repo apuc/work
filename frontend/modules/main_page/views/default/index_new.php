eb28df1284efd0b12015f81eaef9b1225424e143
denis <dkhorosh98@gmail.com>
2021-04-21 13:41:37 +0300

    fix photo for hot vacancies

--- frontend/modules/main_page/views/default/index_new.php
+++ frontend/modules/main_page/views/default/index_new.php
@@ -391,7 +391,7 @@ $background_image = $current_country?('..'.$current_country->main_page_backgroun
                                         <?=$vacancy->getMoneyString()?>
 <!--                                        --><?php //if ($vacancy->hot == 1): ?>
                                         <span  class="home-kard_fire">
-                                            <img class="home-kard_fire_img" src="<?=Yii::getAlias('@web')?>images/main_page/cd-burn.svg" alt="">
+                                            <img class="home-kard_fire_img" src="/images/main_page/cd-burn.svg" alt="">
                                             <p class="home-kard_fire_text">Горячая вакансия</p>
                                         </span>
 <!--                                        --><?php //endif; ?>
@@ -426,7 +426,7 @@ $background_image = $current_country?('..'.$current_country->main_page_backgroun
                             <img class="block-company__single-company__image" src="https://i.pinimg.com/originals/d3/71/d1/d371d1231aafbc9703426615aaca3da8.jpg">
                             <p class="block-company__single-company_description">
                                 <span class="block-company__single-company_description-color"><?=$company['vacancies_count']?> вакансий</span>
-                                компании
+                                <span class="company__text">компании</span>
                             </p>
                         </a>
                         <?php endforeach; ?>
