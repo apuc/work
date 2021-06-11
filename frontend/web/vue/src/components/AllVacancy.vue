bae7dd2d315fdd19379a607a97152018b8320752
denis <dkhorosh98@gmail.com>
2021-06-11 12:32:20 +0300

    фикс условий вывода блоков бесплатных вакансий 2

--- frontend/web/vue/src/components/AllVacancy.vue
+++ frontend/web/vue/src/components/AllVacancy.vue
@@ -53,7 +53,7 @@
     </v-subheader>
     <template v-if="getAllVacancy.length === 0">
       <div class='vacancy__container_empty' v-if="getAllVacancy.length === 0">
-        <div class="resume__item free__vacancy" v-if="getAllVacancy.length<2">
+        <div class="resume__item free__vacancy" v-if="getAllVacancy.length<2 || (timestemp === null && timestemp < Date.now()/1000)">
           <div class="resume__actions" style="margin-top: 74px;">
             <div class="resume__actions_group">
               <div class="resume__actions__item"><img :src="crownIcon" alt="" class="actions_icons">Сделать <b> вакансией дня</b></div>
@@ -75,7 +75,7 @@
             </router-link>
           </div>
         </div>
-        <div class="resume__item add__vacancy" >
+        <div class="resume__item add__vacancy" v-if="timestemp === null || timestemp < Date.now()/1000">
           <h2 class="add__vacancy__title">ДОБАВИТЬ ЕЩЁ ВАКАНСИЮ</h2>
           <div v-if="vacancyCreate===0"><span style="color:#dd3d34;font-weight: 600;">Лимит вакансий исчерпан.</span>
             <span style="font-weight: 600;" v-if="servicePrice[2]">Цена дополнительной вакансии {{ servicePrice[2].price }} руб.</span>
