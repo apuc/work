bff33a5c7a1fc5dd54392f5622310e4b49b1c9c9
alex korona <alex.korona.job@gmail.com>
2021-10-28 13:12:00 +0300

    created ApplicationService

---
+++ frontend/Services/ApplicationService.php
@@ -0,0 +1,30 @@
+<?php
+
+use common\models\User;
+use dektrium\user\helpers\Password;
+
+class ApplicationService
+{
+    /** @var User */
+    public $user;
+
+    /**
+     * Логинит юзера, возвращает true при успехе, false при неудаче.
+     * Сохраняет в себя user, даже если он null.
+     *
+     * @param $username
+     * @param $password
+     * @return bool
+     */
+    public function login($username, $password): bool
+    {
+        $this->user = User::find()->where(['username' => $username])->one();
+
+        if(isset($this->user) && Password::validate($password, $this->user->password_hash)){
+            Yii::$app->user->login($this->user);
+            return true;
+        }
+
+        return false;
+    }
+}
