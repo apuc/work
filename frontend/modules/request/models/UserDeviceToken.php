8e1f56a585232f8f0c0c9e7647e34400428cdd9b
alex korona <alex.korona.job@gmail.com>
2021-11-02 12:44:31 +0300

    created UserDeviceToken

---
+++ frontend/modules/request/models/UserDeviceToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace frontend\modules\request\models;
+
+use DateTimeInterface;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "vacancy".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $device_id unique
+ * @property string $access_token unique | Токен доступа
+ * @property DateTimeInterface $access_token_expired_time Срок действительности токена доступа
+ * @property string $refresh_token unique | Токен обновления для токена доступа
+ * @property DateTimeInterface $refresh_token_expired_time Срок действительности токена обновления
+ **/
+class UserDeviceToken extends ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user_device_token';
+    }
+}
