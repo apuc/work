33bd85000b5028b57f4e99cfc654bcc5d1a0ba44
alex korona <alex.korona.job@gmail.com>
2021-11-04 11:43:26 +0300

    add regenerateAccessToken()

--- frontend/services/TokenService.php
+++ frontend/services/TokenService.php
@@ -9,25 +9,71 @@ use yii\base\Exception;
 
 class TokenService
 {
+    const ACCESS_TOKEN_EXPIRE_TIME = 86400; // день
+    const REFRESH_TOKEN_EXPIRE_TIME = 2592000; // месяц
+
+
     /**
+     * @param User $user
+     * @param string $device_id
      * @throws Exception
-     *
-     * Вызывается при логине для регенерации токенов под девайс
      */
     public function generateNewTokens(User $user, string $device_id)
     {
         $token = UserDeviceToken::findOne(['user_id' => $user->id, 'device_id' => $device_id]);
 
-        if(!isset($token)){
+        if (!isset($token)) {
             $token = new UserDeviceToken();
             $token->user_id = $user->getId();
             $token->device_id = $device_id;
         }
-
-        $token->access_token = Yii::$app->getSecurity()->generateRandomString();
-        $token->access_token_expiration_time = time() + 86400; //плюс сутки
-        $token->refresh_token = Yii::$app->getSecurity()->generateRandomString();
-        $token->access_token_expiration_time = time() + 30 * 86400; //плюс месяц
+        $this->generateAccessToken($token);
+        $this->generateRefreshToken($token);
         $token->save();
+    }
+
+    /**
+     * @param User $user
+     * @param string $device_id
+     * @param string $refresh_token
+     * @return false|UserDeviceToken|null
+     * @throws Exception
+     */
+    public function regenerateAccessToken(User $user, string $device_id, string $refresh_token)
+    {
+        $token = UserDeviceToken::findOne(['user_id' => $user->id, 'device_id' => $device_id]);
+
+        if (!isset($token)) {
+            if ($refresh_token === $token->refresh_token) {
+                $this->generateAccessToken($token);
+                $token->save();
+
+                return $token;
+            }
+
+            return false; //todo log: "trying to get access token with wrong refresh token"
+        }
+
+        return false; //todo log?
+    }
+
+    /**
+     * @param UserDeviceToken $token
+     * @throws Exception
+     */
+    private function generateAccessToken(UserDeviceToken $token)
+    {
+        $token->access_token = Yii::$app->getSecurity()->generateRandomString();
+        $token->access_token_expiration_time = time() + self::ACCESS_TOKEN_EXPIRE_TIME;
+    }
+
+    /**
+     * @param UserDeviceToken $token
+     * @throws Exception
+     */
+    private function generateRefreshToken(UserDeviceToken $token)
+    {
+        $token->refresh_token = Yii::$app->getSecurity()->generateRandomString();
+        $token->access_token_expiration_time = time() + self::REFRESH_TOKEN_EXPIRE_TIME;
     }
 }
