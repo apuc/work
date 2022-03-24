2e782cba539f1dd289417983a6abcf6b5cb8e9f9
alex korona <alex.korona.job@gmail.com>
2022-03-24 23:20:35 +0300

    add test api controller

---
+++ frontend/modules/request/controllers/api/v1/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace frontend\modules\request\controllers\api\v1;
+
+class TestController extends ApiParentController
+{
+
+    protected function verbs()
+    {
+        return [
+            'test' => ['GET'],
+        ];
+    }
+
+    public function actionTest()
+    {
+        return $this->asJson(\Yii::$app->getRequest());
+    }
+}
