6ea0f1f3b50beb53affe6da28410603afbb45edf
alex korona <alex.korona.job@gmail.com>
2022-03-24 13:35:47 +0300

    create auth filter for api-requests

---
+++ frontend/modules/request/filters/AccessTokenAuth.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace frontend\modules\request\filters;
+
+use frontend\services\ApplicationService;
+use yii\filters\auth\HttpBearerAuth;
+use yii\web\UnauthorizedHttpException;
+
+/**
+ * Фильтр для аутентификации API-запросов
+ *
+ * @author Alex Korona ой зря я это подписал...
+ */
+class AccessTokenAuth extends HttpBearerAuth
+{
+    /**
+     * @var ApplicationService $applicationService
+     */
+    private $applicationService;
+
+    /**
+     * @throws UnauthorizedHttpException
+     */
+    public function beforeAction($action)
+    {
+        return parent::beforeAction($action);
+    }
+
+    public function init()
+    {
+        parent::init();
+        $this->applicationService = new ApplicationService();
+    }
+
+    public function authenticate($user, $request, $response)
+    {
+        $authHeader = $request->getHeaders()->get($this->header);
+
+        if ($authHeader !== null) {
+            if ($this->pattern !== null) {
+                if (preg_match($this->pattern, $authHeader, $matches)) {
+                    $authHeader = $matches[1];
+                } else {
+                    return null;
+                }
+            }
+
+
+            $identity = $this->applicationService->loginByAccessToken($authHeader);
+            if ($identity === null) {
+                $this->challenge($response);
+                $this->handleFailure($response);
+            }
+
+            return $identity;
+        }
+
+        return null;
+    }
+}
